4e18934f5bfbffc88c0bb28b45caeb26c73acf99
Akaay <akaay9@gmail.com>
2022-03-06 23:12:26 +0100

    new

---
+++ learningSolidity_1.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWorld {
+    uint sum;
+
+     constructor(uint _sum) {
+    
+    }
+    function AddSomething(uint _a) private view {
+    }
+
+    function sayHello(uint _b) private view {
+    }
+
+}
